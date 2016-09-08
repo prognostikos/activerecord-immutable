116ec4b2c3b7ecd6a3b00c74d41fa9668eb5aa93
nkeyes <nathan.keyes@fooda.com>
2016-09-08 14:15:54 -0500

    remove silence_stream prevent delete_all only raise error on save when there are changes prevent update_all

--- lib/active_record/immutable.rb
+++ lib/active_record/immutable.rb
@@ -3,25 +3,39 @@ require "active_record/immutable/version"
 module ActiveRecord
   module Immutable
     def self.included(model)
-      model.before_destroy :raise_error if model.respond_to?(:before_destroy)
+      model.before_destroy :raise_read_only_error if model.respond_to?(:before_destroy)
+
+      model.class_eval do
+        def self.raise_read_only_error
+          raise ActiveRecord::ReadOnlyRecord
+        end
+
+        def self.delete_all
+          raise_read_only_error
+        end
+
+        def self.update_all(updates)
+          raise_read_only_error
+        end
+      end
     end
 
     def readonly?
-      persisted?
+      changes.present? && persisted?
     end
 
     def touch
-      raise_error
+      raise_read_only_error
     end
 
     def delete
-      raise_error
+      raise_read_only_error
     end
 
     private
 
-    def raise_error
-      raise ActiveRecord::ReadOnlyRecord
+    def raise_read_only_error
+      self.class.raise_read_only_error
     end
   end
 end
